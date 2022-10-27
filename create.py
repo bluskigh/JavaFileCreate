302a939490cc50256fe8c168ae55f8509915983f
Mario Molinito <bell@marios-air.mynetworksettings.com>
2022-10-27 14:17:17 -0400

    Added static keyword to main method

--- create.py
+++ create.py
@@ -4,7 +4,7 @@ import sys
 def run():
     program_name = sys.argv[1]
     with open(f'{program_name}.java', 'w') as f:
-        f.write(f"public class {program_name}" + " {\n\tpublic void main(String[] args) {\n\t}\n}")
+        f.write(f"public class {program_name}" + " {\n\tpublic static void main(String[] args) {\n\t}\n}")
 
 if __name__ == "__main__":
     run()
